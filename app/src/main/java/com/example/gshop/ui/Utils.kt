13aa68ab311a1584b4e07f89a1031d689c6a63ed
Ogg <ogg.developer@gmail.com>
2022-07-12 15:29:41 +0200

    Refactored keyboardListener

--- app/src/main/java/com/example/gshop/ui/Utils.kt
+++ app/src/main/java/com/example/gshop/ui/Utils.kt
@@ -14,33 +14,20 @@ import com.example.gshop.model.doNavigateBack
 import com.example.gshop.redux.Dispatch
 
 @Composable
-fun OnKeyboardClose(view: View, lambda: () -> Unit) {
-    var isKeyboardOpened = remember { false }
+fun OnKeyboardClose(view: View, lambda: () -> Unit) =
+    KeyboardVisibilityChangeListener(view) { isOpened -> if (!isOpened) lambda() }
 
-    fun setKeyboardClosed() {
-        val lastIsKeyboardOpened = isKeyboardOpened
-        isKeyboardOpened = false
-        // If the keyboard was opened but is now closed, call the lambda
-        if (lastIsKeyboardOpened) {
-            lambda()
-        }
-    }
-
-    fun setKeyboardOpened() {
-        isKeyboardOpened = true
-    }
+@Composable
+fun KeyboardVisibilityChangeListener(view: View, onChange: (isOpened: Boolean) -> Unit) {
+    var wasKeyboardOpened = remember { false }
 
     DisposableEffect(view) {
         val onGlobalListener = ViewTreeObserver.OnGlobalLayoutListener {
-            val rect = Rect()
-            view.getWindowVisibleDisplayFrame(rect)
-            val screenHeight = view.rootView.height
-            val keypadHeight = screenHeight - rect.bottom
-            println("keypadHeight: $keypadHeight")
-            if (keypadHeight <= screenHeight * 0.15) {
-                setKeyboardClosed()
-            } else {
-                setKeyboardOpened()
+            val isKeyboardOpened = view.hasKeyboard()
+            if (isKeyboardOpened != wasKeyboardOpened) {
+                // Keyboard visibility has changed
+                wasKeyboardOpened = isKeyboardOpened
+                onChange(isKeyboardOpened)
             }
         }
         view.viewTreeObserver.addOnGlobalLayoutListener(onGlobalListener)
@@ -49,6 +36,14 @@ fun OnKeyboardClose(view: View, lambda: () -> Unit) {
             view.viewTreeObserver.removeOnGlobalLayoutListener(onGlobalListener)
         }
     }
+}
+
+private fun View.hasKeyboard(): Boolean {
+    val rect = Rect()
+    getWindowVisibleDisplayFrame(rect)
+    val screenHeight = rootView.height
+    val keypadHeight = screenHeight - rect.bottom
+    return keypadHeight > screenHeight * 0.15
 }
 
 @Composable
