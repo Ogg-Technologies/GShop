ad1882aa5a03e93b76dd3a429bfce5af73ee145d
Ogg <ogg.developer@gmail.com>
2022-07-13 22:15:51 +0200

    Changed theme

--- app/src/main/java/com/example/gshop/ui/theme/Theme.kt
+++ app/src/main/java/com/example/gshop/ui/theme/Theme.kt
@@ -3,14 +3,15 @@ package com.example.gshop.ui.theme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun GShopTheme(content: @Composable () -> Unit) {
     MaterialTheme(
         colors = darkColors(
-            primary = Purple200,
-            primaryVariant = Purple700,
-            secondary = Teal200
+            primary = Color(0xFF9575CD),
+            primaryVariant = Color(0xFF512DA8),
+            secondary = Color(0xFF66BB6A)
         ),
         typography = Typography,
         shapes = Shapes,
